64143851d770e5ca187c74f097892f5c2a8320a9
Richard T. Miles <RichardTMiles@users.noreply.github.com>
2018-02-04 12:55:48 -0600

    slightly minified to ECMA 6

--- loadJS.js
+++ loadJS.js
@@ -1,6 +1,6 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
-(function( w ){
-	var loadJS = function( src, cb ){
+((( w )=>{
+	var loadJS = ( src, cb )=>{
 		"use strict";
 		var ref = w.document.getElementsByTagName( "script" )[ 0 ];
 		var script = w.document.createElement( "script" );
@@ -19,4 +19,4 @@
 	else {
 		w.loadJS = loadJS;
 	}
-}( typeof global !== "undefined" ? global : this ));
+})( typeof global !== "undefined" ? global : this ));
